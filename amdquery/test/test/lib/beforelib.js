8284fa49f859f521dedc6ac28474549204d37fd6
caojun-mac <78612846@qq.com>
2014-04-13 19:00:52 +0800

    This is generated by build. Lastest Commit:e8b97e6f411d5ece18e655c1b793fcf29ea17128 Message:Modify Test.js. Date:Sun Apr 13 18:56:57 2014 +0800

---
+++ amdquery/test/test/lib/beforelib.js
@@ -0,0 +1,19 @@
+
+  var applicationCache = null;
+  
+var aQueryConfig = {
+	amdquery: {
+		define: "$",
+		loadingImage: "assets/images/welcome.gif"
+	},
+	amd: {
+		detectCR: 1
+	},
+	module: {
+		compatibleEvent: 1
+	},
+	app: {
+		src: "../document/app",
+		debug: 1
+	}
+};
